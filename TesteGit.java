9eb2d683da5bcdf4b89294e61ce10f08023abf1f
FilemonJunior <filajr@gmail.com>
2015-10-20 10:50:18 -0200

    Commit tst

--- TesteGit.java
+++ TesteGit.java
@@ -2,7 +2,6 @@ public class TesteGit {
     public static void main(String args[]){
         System.out.println("Exemplo de teste de uso do git");
         System.out.println("Exemplo de alteração - pull");
-        System.out.println("Exemplo de alteração - branch");
-        System.out.println("Exemplo de alteração - Branch Work");
+        System.out.println("Exemplo de alteração - branch master");
     }
 }
